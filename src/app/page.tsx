563fd8c347b0bafdb7d8c62d32f608d535de0af0
Rui <s1F102501866@iniad.org>
2025-11-28 12:21:45 +0900

    feat: Empty Board

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 'use client'
 
 import { useState, useEffect } from "react";
+import {Board} from "../components/Board/Board"
+
 
 export default function Home() {
   return (
+    
     <div className="container">
+      <h1>マインスイーパーver2.0!</h1>
+      <Board></Board>
     </div>
   );
 }
---
+++ src/components/Board/Board.tsx
@@ -0,0 +1,10 @@
+import styles from "./Board.module.css"
+
+export function Board() {
+  
+  return (
+    <div className={styles.Board}>
+
+    </div>
+  )
+}
